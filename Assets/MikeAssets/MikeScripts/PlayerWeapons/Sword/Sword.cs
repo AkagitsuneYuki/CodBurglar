b469bad4dc31fbc2d730c0f04b2436d3f4e6656a
AkagitsuneYuki <michael.lovallo@gmail.com>
2020-02-23 15:26:03 -0500

    Push for Sean

---
+++ Assets/MikeAssets/MikeScripts/PlayerWeapons/Sword/Sword.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sword : MonoBehaviour
+{
+
+    [SerializeField] private Animator anime;
+    [SerializeField] private CapsuleCollider hitbox;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if (anime.GetBool("Swing") == false)
+            {
+                anime.SetBool("Swing", true);
+            }
+        }
+    }
+
+    private void SwingEnd()
+    {
+        StartCoroutine(Swing());
+    }
+
+    IEnumerator Swing()
+    {
+        yield return new WaitForSeconds(0.1f);
+        anime.SetBool("Swing", false);
+    }
+}
